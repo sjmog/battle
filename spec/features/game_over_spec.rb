4f532505adb39b98409b846a524513ac2e4caca6
Sam Morgan <s_morgan@me.com>
2015-10-20 18:58:11 +0100

    User Story 11
    As Player 1,
    So I can win a game of Battle,
    I want to see a 'win' message if Player 2 reaches 0HP first
    
    No extra code needed - just a test!

--- spec/features/game_over_spec.rb
+++ spec/features/game_over_spec.rb
@@ -4,11 +4,23 @@ feature 'Game over' do
       sign_in_and_play
       attack_and_confirm
       allow(Kernel).to receive(:rand).and_return(60)
+      click_button 'Attack'
     end
 
     scenario 'Player 1 loses' do
-      click_button 'Attack'
       expect(page).to have_content 'Dave loses!'
+    end
+  end
+
+  context 'when Player 2 reaches 0 HP first' do
+    before do
+      sign_in_and_play
+      allow(Kernel).to receive(:rand).and_return(60)
+      click_button 'Attack'
+    end
+
+    scenario 'Player 2 loses' do
+      expect(page).to have_content 'Mittens loses!'
     end
   end
 end
