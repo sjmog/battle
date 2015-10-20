1ccc3f371ed17bd3e96db55cfedb150188abcccc
Sam Morgan <s_morgan@me.com>
2015-10-20 16:06:15 +0100

    (WIP) User Story 4
    As Player 1,
    So I can start to win a game of Battle,
    I want my attack to reduce Player 2's HP by 10
    
    > Next, you will need to refactor to use a Model for storing the state of your Players

--- spec/features/attack_spec.rb
+++ spec/features/attack_spec.rb
@@ -1,11 +1,21 @@
-# As Player 1,
-# So I can win a game of Battle,
-# I want to attack Player 2, and I want to get a confirmation
-
 feature 'Attacking' do
+  # As Player 1,
+  # So I can win a game of Battle,
+  # I want to attack Player 2, and I want to get a confirmation
   scenario 'attack Player 2' do
     sign_in_and_play
     click_button 'Attack'
     expect(page).to have_content 'Dave attacked Mittens'
+  end
+
+  # As Player 1,
+  # So I can start to win a game of Battle,
+  # I want my attack to reduce Player 2's HP by 10
+  scenario 'reduce Player 2 HP by 10' do
+    sign_in_and_play
+    click_button 'Attack'
+    click_link 'OK'
+    expect(page).not_to have_content 'Mittens: 60HP'
+    expect(page).to have_content 'Mittens: 50HP'
   end
 end
