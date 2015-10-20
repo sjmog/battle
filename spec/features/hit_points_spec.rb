d5289ab67bb0c7b45a5d866a4915ec51d9afcc31
Sam Morgan <s_morgan@me.com>
2015-10-20 16:56:15 +0100

    User Story 6
    As Player 1,
    So I can see how close I am to losing,
    I want to see my own hit points

--- spec/features/hit_points_spec.rb
+++ spec/features/hit_points_spec.rb
@@ -1,10 +1,18 @@
-# As Player 1,
-# So I can see how close I am to winning
-# I want to see Player 2's Hit Points
-
 feature 'View hit points' do
+
+  # As Player 1,
+  # So I can see how close I am to winning
+  # I want to see Player 2's Hit Points
   scenario 'see Player 2 hit points' do
     sign_in_and_play
     expect(page).to have_content 'Mittens: 60HP'
+  end
+
+  # As Player 1,
+  # So I can see how close I am to losing,
+  # I want to see my own hit points
+  scenario 'see Player 1 hit points' do
+    sign_in_and_play
+    expect(page).to have_content 'Dave: 60HP'
   end
 end
