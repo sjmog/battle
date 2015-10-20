a66aeb28d8f94db8d9eaef37e2da27a2916bb193
Sam Morgan <s_morgan@me.com>
2015-10-20 16:44:31 +0100

    User Story 5
    As two Players,
    So we can continue our game of Battle,
    We want to switch turns
    
    Allows `Game` to keep track of turns, with a visual indicator.

---
+++ spec/features/switch_turns_spec.rb
@@ -0,0 +1,20 @@
+# As two Players,
+# So we can continue our game of Battle,
+# We want to switch turns
+
+describe 'Switch turns' do
+  context 'seeing the current turn' do
+    scenario 'at the start of the game' do
+      sign_in_and_play
+      expect(page).to have_content "Dave's turn"
+    end
+
+    scenario 'after player 1 attacks' do
+      sign_in_and_play
+      click_button 'Attack'
+      click_link 'OK'
+      expect(page).not_to have_content "Dave's turn"
+      expect(page).to have_content "Mittens's turn"
+    end
+  end
+end
